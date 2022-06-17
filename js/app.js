b848172e67c0a74e7f6909ea80b3378057a87647
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2022-06-17 16:16:30 -0400

    Added basic html layout, css styles, and button function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-// Random Quote Generator: HTML, CSS, 
+// Random Quote Generator: HTML, CSS, JS, API
 
+// Grab elements
+const text = document.getElementById('text');
+const quote = document.getElementById('quote');
+const button = document.getElementById('button');
 
+// Add event listener
+button.addEventListener('click', getQuotes);
 
+// Create getQuotes function
+function getQuotes() {
+  console.log('clicked');
 
+};
 
 // Fetch Request
 fetch("https://type.fit/api/quotes")
