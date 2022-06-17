a605c01de9aa2e0c9b7dd36fa0434b169b41e972
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2022-06-17 16:49:49 -0400

    Revised html layout, and css styles

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 // Grab elements
 const text = document.getElementById('text');
-const quote = document.getElementById('quote');
+const author = document.getElementById('author');
 const button = document.getElementById('button');
 
 // Add event listener
@@ -10,15 +10,21 @@ button.addEventListener('click', getQuotes);
 
 // Create getQuotes function
 function getQuotes() {
-  console.log('clicked');
+  // Fetch Request
+  fetch("https://type.fit/api/quotes")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let textOutput = '';
+      let authorOutput = '';
+      // Loop through data
+      data.forEach(quote => {
+        textOutput += `<p>${quote.text}</p>`
+        authorOutput += `<p>${quote.author}</p>`
+      });
+      // Append to browser
+      text.innerHTML = textOutput;
+      author.innerHTML = authorOutput;
 
-};
-
-// Fetch Request
-fetch("https://type.fit/api/quotes")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+    });
