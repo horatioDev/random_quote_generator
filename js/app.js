d5aee1deee93b7cb9a754b1dedf05d64f9329f27
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2022-06-17 18:02:35 -0400

    Completed revision of html, css, and completed js functionality

--- js/app.js
+++ js/app.js
@@ -1,30 +1,35 @@
 // Random Quote Generator: HTML, CSS, JS, API
 
 // Grab elements
-const text = document.getElementById('text');
-const author = document.getElementById('author');
-const button = document.getElementById('button');
+const getQuoteButton = document.getElementById('button');
 
 // Add event listener
-button.addEventListener('click', getQuotes);
+getQuoteButton.addEventListener('click', getQuote);
 
 // Create getQuotes function
-function getQuotes() {
-  // Fetch Request
-  fetch("https://type.fit/api/quotes")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      let textOutput = '';
-      let authorOutput = '';
-      // Loop through data
-      data.forEach(quote => {
-        textOutput += `<p>${quote.text}</p>`
-        authorOutput += `<p>${quote.author}</p>`
-      });
-      // Append to browser
-      text.innerHTML = textOutput;
-      author.innerHTML = authorOutput;
+async function getQuote() {
+  //api for quotes
+  var url = "https://type.fit/api/quotes";
 
-    });
+  // fetch the data from api
+  const response = await fetch(url);
+
+  //  Conver response to JSON array
+  const quotes = await response.json();
+
+  // Use Math.floor to be to select one item at a time from array
+  const idx = Math.floor(Math.random() * quotes.length);
+
+  // Store each element from object to a variable for use
+  const quote = quotes[idx].text;
+  const author = quotes[idx].author;
+
+  let output = '';
+  output += `<h1>${quote}</h1>
+  <p>${author}</p>`;
+
+  document.getElementById('output').innerHTML = output;
+
+};
+
+getQuote();
